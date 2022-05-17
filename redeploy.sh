2241354a2af1384f0b7402e5e8bc762b0405f105
Ted Piotrowski <tppiotrowski@gmail.com>
2022-05-17 19:06:53 +0000

    Force kill previous process

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Killing server"
-kill $(ps -aux | grep "node dist/og-image-server.js" | awk '{print $2; exit}')
+kill -9 $(ps -aux | grep "node dist/og-image-server.js" | awk '{print $2; exit}')
 echo "Building new binary"
 yarn build
 echo "Starting server"
